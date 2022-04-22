5d2dac3708f82896db6e75b246c40be8c417f1d8
lottestockm <55782954+lottestockm@users.noreply.github.com>
2022-04-22 10:29:16 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,46 @@
 // JavaScript Document
-console.log("Howdy!");
+// console.log(selectedVids);
+
+// let video = document.querySelector('video');
+// let button = document.querySelector('#pwr');
+
+// pwr.addEventListener('click', () => {
+//     video.src ='videos/mickey2004.mov'; })
+
+
+var video = document.querySelector('video');
+var button = document.getElementById('pwr')
+
+function uitzetten(){
+    if(video.style.opacity == '1'){
+        video.style.opacity = '0';
+        video.pause();
+    }else{
+        video.style.opacity = '1';
+        video.play();
+    }
+    
+}
+console.log(vids);
+// let selectedVids = ['mickey1928.mov', 'mickey1937.mov', 'mickey1940.mov', 'mickey1983.mov', 'mickey2004.mov','mickey2016.mov'];
+// console.log(selectedVids);
+
+var slider_vid = document.querySelector('video');
+var vids = ['mickey1928.mov', 'mickey1937.mov', 'mickey1940.mov', 'mickey1983.mov', 'mickey2004.mov','mickey2016.mov'];
+var i = 0; //current  Image index
+
+function prev(){
+    if(i <= 0 ) i = vids.length;
+    i--;
+    return setVid();
+}
+
+function next(){
+    if ( i >= vids.length-1 ) i = -1;
+    i++;
+    return setVid();
+}
+
+function setVid(){
+    return slider_vid.setAttribute('src','videos/' + vids[i]);
+}
